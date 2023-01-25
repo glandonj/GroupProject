88954e3b71d88a48c4936af4f1a3583e6925234f
john <glandonj@gmail.com>
2023-01-25 17:03:38 -0500

    Added foreach and cw name and age

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -3,10 +3,17 @@
 using System.ComponentModel;
 using GroupProject;
 
-List<Person> team = new List<Person>();
+List<Person> team = new List<Person>()
 {
-    new Person("John G", 38);
-    new Person("Josh S", 41);
-    new Person("Earle F", 54);
+    new Person("John G", 38),
+    new Person("Josh S", 41),
+    new Person("Earle F", 54)
+};
+
+foreach(Person p in team)
+{
+    Console.WriteLine(p.Name);
+    Console.WriteLine(p.Age);
 }
 
+Console.ReadLine();
