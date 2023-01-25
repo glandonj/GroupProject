3e5dc30c4c7fa9e26736d60f338a51e6726f8e7e
jstrathres <jstraz7@gmail.com>
2023-01-25 16:16:00 -0500

    added class

---
+++ GroupProject/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    public class Person
+    {//properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        //constructor
+        public Person(string _name, int _age)
+        {
+            Name = _name;
+            Age = _age;
+        }
+        //Method    
+
+            
+    }
+}
